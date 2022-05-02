0fd4e8c85735b5f5664e5af36374435e1dc61573
alex hernandez <alexanderohernandez21@gmail.com>
2022-05-01 20:14:09 -0700

    added employees table

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,12 @@ VALUES("Sales Lead", 100000, 1),
         ("Legal Team Lead", 250000, 4),
         ("Lawyer", 190000, 4);
 
+INSERT INTO employees(first_name, last_name, position_id, manager_id)
+VALUES("John", "Doe", 1 , NULL),
+        ("Mike", "Chan", 2 , 1),
+        ("Ashley", "Rodriguez", 3 ,NULL),
+        ("Kevin", "Tupik", 4 , 3),
+        ("Kunal", "Sing", 5 , NULL),
+        ("Malia", "Brown", 6 , 5),
+        ("Sarah", "Lourd", 7 , NULL),
+        ("Tom", "Allen", 8 , 7);
